9de7595d634bad444d4e4449ba032adfc9e9b68b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-12-23 11:59:00 +0100

    corrijo ej 2

--- ejercicio2.py/main.py
+++ ejercicio2.py/main.py
@@ -1,7 +1,6 @@
 from contador import *
-if __name__ == "main":
+if __name__ == "__main__":
     crear_fichero()
     escribir_fichero("inc")
-    escribir_fichero("dec")
-    escribir_fichero("inc")
-    escribir_fichero()
+    escribir_fichero("dec")  #hay que ir ejecutando la funcion de escribir fichero 1 a 1 para que funcione
+    
